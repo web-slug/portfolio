41330f6ea2181788bccc3b957a8c45d6fc4f4b57
sourabhkahar <85325254+sourabhkahar@users.noreply.github.com>
2021-11-28 14:31:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primarycolor:'#686a6d'
+      },
+    },
+    
+    containerWrap: {
+        'margin': '2rem'
+    },
+    fontFamily: {
+      sans: ['Amatic SC', 'sans-serif'],
+     // serif: ['Merriweather', 'serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
